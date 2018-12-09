315c44b54167a0f88d7beb1612fab5c874a36af5
Ybsoft <youssefbousfoul@gmail.com>
2018-12-09 11:27:19 +0100

    first step - design

---
+++ swooshapp2/swooshapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swooshapp2
+//
+//  Created by YoussefBSF on 12/9/18.
+//  Copyright © 2018 aimads-maroc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
